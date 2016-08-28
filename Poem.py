2f27258b197c8a8429930c75c0666ff89b82b778
scottshull <sshull@students.mchenry.edu>
2016-08-28 18:53:52 -0500

    Add files via upload

---
+++ Poem.py
@@ -0,0 +1,48 @@
+# Variables
+verb = "Crunch"
+object = "cheeto"
+noun = "pool"
+object2 = "crumb"
+adjective = "hungry"
+noun2 = "dog"
+verb2 = "asleep"
+pronoun = "her"
+adjective2 = "big"
+noun3 = "shoe"
+noun4 = "man"
+adjective3 = "tasty"
+color = "green"
+verb3 = "yell"
+
+# poem
+print(verb + ", " + verb + "," + " little " + object + ",")
+print("How I wonder where you are" + "!")
+print("Up above the " + noun + " so high" + ",")
+print("Like a " + object2 + " in the sky" + ",\n")
+
+print("When the " + adjective + " " + noun2  +" is gone" + ",")
+print("When the nothing shines upon" + ",")
+print("Then you show " + pronoun +  " " + adjective2 + " " + noun3 + ",")
+print(verb + ", " + verb + "," + " all night" + ",\n")
+
+print("Then the " + noun4 + "in the dark")
+print("Thanks you for your " + adjective3 + " sparks" ";")
+print("He could not see which way to go " + ",")
+print("If you did not " + verb + " so" ".\n")
+
+print("In the dark " + color + " sky,")
+print("And often through my curtains " + verb3 + ",")
+print("For you never shut your eye")
+print("Till the sun is in the sky" ".\n")
+
+print("As bright as your " + adjective2 + " spark")
+print("Lights the " + noun4 + " in the dark,")
+print("Though I know what you are,")
+print(verb + ", " + verb + "," + " little " + object + ".")
+
+
+
+
+
+
+
